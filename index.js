498cfe304295d9f7b68806116c69e3f61c04747a
CubeBeveled <cubebeveled@gmail.com>
2024-10-19 15:07:44 +0200

    This is it

---
+++ index.js
@@ -0,0 +1,16 @@
+const socks = require("socksv5");
+const port = 25490;
+
+const server = socks.createServer((info, accept, deny) => {
+  accept();
+});
+
+server.on("error", (err) => {
+  console.log(color.red("SOCKS5 Server Error:"), err);
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(color.green(`SOCKS5 server running on port ${port}`));
+});
+
+server.useAuth(socks.auth.None());
